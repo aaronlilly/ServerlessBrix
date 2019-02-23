327344af01e8b337a2995b3fe4b2b62f4329947c
Aaron Lilly <aaronlilly84@gmail.com>
2019-02-22 22:22:13 -0500

    fireimports

--- Preselects/scripts/ninjascript.js
+++ Preselects/scripts/ninjascript.js
@@ -21,32 +21,31 @@
                         }            
                               });
             });
-                 
                    //
-// $(document).ready(function () {
-//     $('#search-now').click(function () {
-//         $.ajax
-//             ({
-//                 method: "GET",
-//                 url: "https://aaronlilly.github.io/ServerlessBrix/SLBr-master/apis/FakeLego2.json"
-//             }).done(function (data) {
-//                 for (var i = 0; i < data.results.length; i++) {
-//                     var HRF = "<a href='";
-//                     var ND = "'>";
-//                     var AA = "</a>"
-//                     var YES = '<img src="https://www.lego.com/biassets/biimg/6237212.png">';
-//                     var IGG = '<img src="';
-//                     var LST = '">'
+$(document).ready(function () {
+    $('#search-now').click(function () {
+        $.ajax
+            ({
+                method: "GET",
+                url: "https://aaronlilly.github.io/PokeApp/api/v2/pokemon/FakeLego2.json"
+            }).done(function (data) {
+                for (var i = 0; i < data.results.length; i++) {
+                    var HRF = "<a href='";
+                    var ND = "'>";
+                    var AA = "</a>"
+                    var YES = '<img src="https://www.lego.com/biassets/biimg/6237212.png">';
+                    var IGG = '<img src="';
+                    var LST = '">'
                     
-//                     //data.results[i].URL
+                    //data.results[i].URL
 
 
-//                     //$("#legoList2").append(HRF + data.results[i].URL + ND + IGG + data.results[i].Images + LST + AA);
-//                     $("#legoList2").append(data.results[i].URL);
-//                 }
-//             });
-//     });
-// });
+                    //$("#legoList2").append(HRF + data.results[i].URL + ND + IGG + data.results[i].Images + LST + AA);
+                    $("#legoList2").append(data.results[i].URL);
+                }
+            });
+    });
+});
 
 
 ///
