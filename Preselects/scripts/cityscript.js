fe6c1baa95e30d4ce26736d8ef06c5d38dea132d
Aaron Lilly <38325891+aaronlilly@users.noreply.github.com>
2019-02-25 17:05:32 -0500

    moving to solution

--- Preselects/scripts/cityscript.js
+++ Preselects/scripts/cityscript.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     $.ajax
         ({
             method: "GET",
-            url: "https://aaronlilly.github.io/ServerlessBrix/Preselects/xls/json/city2.json"
+            url: "https://aaronlilly.github.io/ServerlessBrix/Preselects/xls/json/city.json"
         }).done(function (data) {
             for (var i = 0; i < data.results.length; i++) {
                 var HRF = "<a href='";
