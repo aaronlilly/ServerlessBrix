f2ef42ad2879039af57404bd902bfca5465abd36
Aaron Lilly <aaronlilly84@gmail.com>
2019-02-24 22:16:47 -0500

    i now have an array filter, now to apply it to an external api

--- scripts/ninjascript.js
+++ scripts/ninjascript.js
@@ -7,6 +7,8 @@
                     url: "https://aaronlilly.github.io/PokeApp/api/v2/pokemon/FakeLego2.json"
                          }).done(function(data) 
 
+
+
                                {
                         for (var i = 0; i < data.results.length; i++) 
                         {
@@ -16,7 +18,7 @@
                             var YES = '<img src="https://www.lego.com/biassets/biimg/6237212.png">';
                             var IGG = '<img src="';
                             var LST = '">'
-
+                                 var Datarr = [data.results];
                             $("#legoList").append(HRF + data.results[i].URL + ND + IGG + data.results[i].Images +LST + AA);
                         }            
                               });
@@ -48,5 +50,44 @@
 //     });
 // });
 
+
+<script type="text/javascript">
+    var obj = {
+      'results': [{
+            "ID": 2924,
+      "SetNumber": 7942,
+      "URL": "https://www.lego.com/biassets/bi/4586972.pdf",
+      "Images": "https://www.lego.com/biassets/biimg/4586972.png",
+      "Description": "Off Road Fire Rescue",
+      "DashedNumber": "7942-1",
+      "Preselect": "fire"
+        }, {
+              "ID": 4470,
+      "SetNumber": 7942,
+      "URL": "https://www.lego.com/biassets/bi/4656089.pdf",
+      "Images": "https://www.lego.com/biassets/biimg/4656089.png",
+      "Description": "Police Boat",
+      "DashedNumber": "7287-1 ",
+      "Preselect": "police"
+        },
+           
+    ]
+};
+
+
+// (Note that because `price` and such are given as strings in your object,
+// the below relies on the fact that <= and >= with a string and number
+// will coerce the string to a number before comparing.)
+var newArray = obj.results.filter(function (el) {
+  return el.ID ==  2924 &&
+         el.SetNumber &&
+         el.URL &&
+         el.Images &&
+         el.Description &&
+         el.DashedNumber &&
+         el.Preselect ; 
+});
+console.log(newArray);
+</script>
 
 ///
