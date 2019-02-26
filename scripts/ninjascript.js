22672119bbdb12b1e0ecb5cdc88d517d01dc82b5
Aaron Lilly <38325891+aaronlilly@users.noreply.github.com>
2019-02-25 20:10:04 -0500

    Update ninjascript.js
    maybe

--- scripts/ninjascript.js
+++ scripts/ninjascript.js
@@ -52,27 +52,44 @@
 
 
 
-    var obj = {
-      'results': [{
-            "ID": 2924,
-      "SetNumber": 7942,
-      "URL": "https://www.lego.com/biassets/bi/4586972.pdf",
-      "Images": "https://www.lego.com/biassets/biimg/4586972.png",
-      "Description": "Off Road Fire Rescue",
-      "DashedNumber": "7942-1",
-      "Preselect": "fire"
-        }, {
-              "ID": 4470,
-      "SetNumber": 7942,
-      "URL": "https://www.lego.com/biassets/bi/4656089.pdf",
-      "Images": "https://www.lego.com/biassets/biimg/4656089.png",
-      "Description": "Police Boat",
-      "DashedNumber": "7287-1 ",
-      "Preselect": "police"
-        },
+//     var obj = {
+//       'results': [{
+//             "ID": 2924,
+//       "SetNumber": 7942,
+//       "URL": "https://www.lego.com/biassets/bi/4586972.pdf",
+//       "Images": "https://www.lego.com/biassets/biimg/4586972.png",
+//       "Description": "Off Road Fire Rescue",
+//       "DashedNumber": "7942-1",
+//       "Preselect": "fire"
+//         }, {
+//               "ID": 4470,
+//       "SetNumber": 7942,
+//       "URL": "https://www.lego.com/biassets/bi/4656089.pdf",
+//       "Images": "https://www.lego.com/biassets/biimg/4656089.png",
+//       "Description": "Police Boat",
+//       "DashedNumber": "7287-1 ",
+//       "Preselect": "police"
+//         },
            
-    ]
-};
+//     ]
+// };
+
+
+
+// var newArray = obj.results.filter(function (el) {
+//   return el.ID ==  2924 &&
+//          el.SetNumber &&
+//          el.URL &&
+//          el.Images &&
+//          el.Description &&
+//          el.DashedNumber &&
+//          el.Preselect ; 
+// });
+// console.log(newArray);
+
+
+///
+ var obj == Datarr ;
 
 
 // (Note that because `price` and such are given as strings in your object,
