516700e36c91a5cb2e0d13e524ef8791b3a270a9
Aaron Lilly <38325891+aaronlilly@users.noreply.github.com>
2019-02-27 09:14:44 -0500

    initial dup

---
+++ scripts/legoarray.js
@@ -0,0 +1,130 @@
+          
+
+            $(document).ready(function ()  
+            {
+                    $.ajax
+                        ({
+                    method: "GET",
+                    url: "https://aaronlilly.github.io/PokeApp/api/v2/pokemon/FakeLego2.json"
+                         }).done(function(data)  
+
+
+
+
+                               {  
+                        for (var i = 0; i < data.results.length; i++) 
+                        {
+                            var HRF = "<a href='";
+                            var ND = "'>";
+                            var AA = "</a>"
+                            var YES = '<img src="https://www.lego.com/biassets/biimg/6237212.png">';
+                            var IGG = '<img src="';
+                            var LST = '">'
+                           
+
+                            $("#legoList2").append(HRF + data.results[i].URL + ND + IGG + data.results[i].Images + LST + AA);
+                        }            
+                              });
+            });
+
+        ;           
+                   
+//  $(document).ready(function () {
+//      $('#SetNumberSearch2').click(function () {
+//          $.ajax
+//              ({
+//                  method: "GET",
+//                  url: "https://aaronlilly.github.io/ServerlessBrix/apis/FakeLego2.json"
+//              }).done(function (data) {
+//                  for (var i = 0; i < data.results.length; i++) {
+//                      var HRF = "<a href='";
+//                      var Datarr = [data.results];
+//                      var UserInput = $('#prodID').val()
+//                      var newArray = Datarr.results.filter(function (el) {
+//                          return el.ID = 2924  &&
+//                              el.SetNumber &&
+//                              el.URL &&
+//                              el.Images &&
+//                              el.Description &&
+//                              el.DashedNumber &&
+//                              el.Preselect;
+//                      });
+//                      console.log(newArray);
+//                      var ND = "'>";
+//                      var AA = "</a>"
+//                      var YES = '<img src="https://www.lego.com/biassets/biimg/6237212.png">';
+//                      var IGG = '<img src="';
+//                      var LST = '">'
+                    
+//                      //data.results[i].URL
+
+
+//                      //$("#legoList2").append(HRF + data.results[i].URL + ND + IGG + data.results[i].Images + LST + AA);
+//                      $("#legoList2").append(newArray);
+//                  }
+//              });
+//      });
+// });
+// //
+$(document).ready(function () {
+$('#search-now').click(function () {
+    alert($('#prodId').val());
+});
+});
+//$('#prodId').val()
+
+
+    var obj = {
+      'results': [{
+            "ID": 2924,
+      "SetNumber": 7942,
+      "URL": "https://www.lego.com/biassets/bi/4586972.pdf",
+      "Images": "https://www.lego.com/biassets/biimg/4586972.png",
+      "Description": "Off Road Fire Rescue",
+      "DashedNumber": "7942-1",
+      "Preselect": "fire"
+        }, {
+              "ID": 4470,
+      "SetNumber": 7942,
+      "URL": "https://www.lego.com/biassets/bi/4656089.pdf",
+      "Images": "https://www.lego.com/biassets/biimg/4656089.png",
+      "Description": "Police Boat",
+      "DashedNumber": "7287-1 ",
+      "Preselect": "police"
+        }
+           
+    ]
+};
+
+
+    $(document).ready(function () {
+$('#dynam-now').click(function () {
+    
+var newArray = obj.results.filter(function (el) {
+  return el.ID ==  $('#dynamId').val() &&
+         el.SetNumber &&
+         el.URL &&
+         el.Images &&
+         el.Description &&
+         el.DashedNumber &&
+         el.Preselect ; 
+});
+console.log(newArray);
+});
+});
+
+
+/////
+////var newArray = Datarr.results.filter(function (el) {
+////  return el.ID ==  UserInput &&
+////         el.SetNumber &&
+////         el.URL &&
+////         el.Images &&
+////         el.Description &&
+////         el.DashedNumber &&
+////         el.Preselect ; 
+////});
+////console.log(newArray);
+
+
+///
