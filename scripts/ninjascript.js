31db7ab9725f4bad66cfb09324c4a53e9d6d56b0
Aaron Lilly <aaronlilly84@gmail.com>
2019-02-26 22:24:55 -0500

    an array append and a filter, just not together

--- scripts/ninjascript.js
+++ scripts/ninjascript.js
@@ -19,91 +19,91 @@
                             var IGG = '<img src="';
                             var LST = '">'
 
-                            $("#legoList").append(HRF + data.results[i].URL + ND + IGG + data.results[i].Images + LST + AA);
+                            $("#legoList2").append(HRF + data.results[i].URL + ND + IGG + data.results[i].Images + LST + AA);
                         }            
                               });
             });
-                 
+        ;           
                    
- $(document).ready(function () {
-     $('#SetNumberSearch2').click(function () {
-         $.ajax
-             ({
-                 method: "GET",
-                 url: "https://aaronlilly.github.io/ServerlessBrix/apis/FakeLego2.json"
-             }).done(function (data) {
-                 for (var i = 0; i < data.results.length; i++) {
-                     var HRF = "<a href='";
-                     var Datarr = [data.results];
-                     var UserInput = $('#prodID').val()
-                     var newArray = Datarr.results.filter(function (el) {
-                         return el.ID = 2924  &&
-                             el.SetNumber &&
-                             el.URL &&
-                             el.Images &&
-                             el.Description &&
-                             el.DashedNumber &&
-                             el.Preselect;
-                     });
-                     console.log(newArray);
-                     var ND = "'>";
-                     var AA = "</a>"
-                     var YES = '<img src="https://www.lego.com/biassets/biimg/6237212.png">';
-                     var IGG = '<img src="';
-                     var LST = '">'
+//  $(document).ready(function () {
+//      $('#SetNumberSearch2').click(function () {
+//          $.ajax
+//              ({
+//                  method: "GET",
+//                  url: "https://aaronlilly.github.io/ServerlessBrix/apis/FakeLego2.json"
+//              }).done(function (data) {
+//                  for (var i = 0; i < data.results.length; i++) {
+//                      var HRF = "<a href='";
+//                      var Datarr = [data.results];
+//                      var UserInput = $('#prodID').val()
+//                      var newArray = Datarr.results.filter(function (el) {
+//                          return el.ID = 2924  &&
+//                              el.SetNumber &&
+//                              el.URL &&
+//                              el.Images &&
+//                              el.Description &&
+//                              el.DashedNumber &&
+//                              el.Preselect;
+//                      });
+//                      console.log(newArray);
+//                      var ND = "'>";
+//                      var AA = "</a>"
+//                      var YES = '<img src="https://www.lego.com/biassets/biimg/6237212.png">';
+//                      var IGG = '<img src="';
+//                      var LST = '">'
                     
-                     //data.results[i].URL
+//                      //data.results[i].URL
 
 
-                     //$("#legoList2").append(HRF + data.results[i].URL + ND + IGG + data.results[i].Images + LST + AA);
-                     $("#legoList2").append(newArray);
-                 }
-             });
-     });
-});
-//
+//                      //$("#legoList2").append(HRF + data.results[i].URL + ND + IGG + data.results[i].Images + LST + AA);
+//                      $("#legoList2").append(newArray);
+//                  }
+//              });
+//      });
+// });
+// //
 $(document).ready(function () {
 $('#search-now').click(function () {
-    alert(newArray);
+    alert($('#prodId').val());
 });
 });
 //$('#prodId').val()
 
 
-//     var obj = {
-//       'results': [{
-//             "ID": 2924,
-//       "SetNumber": 7942,
-//       "URL": "https://www.lego.com/biassets/bi/4586972.pdf",
-//       "Images": "https://www.lego.com/biassets/biimg/4586972.png",
-//       "Description": "Off Road Fire Rescue",
-//       "DashedNumber": "7942-1",
-//       "Preselect": "fire"
-//         }, {
-//               "ID": 4470,
-//       "SetNumber": 7942,
-//       "URL": "https://www.lego.com/biassets/bi/4656089.pdf",
-//       "Images": "https://www.lego.com/biassets/biimg/4656089.png",
-//       "Description": "Police Boat",
-//       "DashedNumber": "7287-1 ",
-//       "Preselect": "police"
-//         },
+    var obj = {
+      'results': [{
+            "ID": 2924,
+      "SetNumber": 7942,
+      "URL": "https://www.lego.com/biassets/bi/4586972.pdf",
+      "Images": "https://www.lego.com/biassets/biimg/4586972.png",
+      "Description": "Off Road Fire Rescue",
+      "DashedNumber": "7942-1",
+      "Preselect": "fire"
+        }, {
+              "ID": 4470,
+      "SetNumber": 7942,
+      "URL": "https://www.lego.com/biassets/bi/4656089.pdf",
+      "Images": "https://www.lego.com/biassets/biimg/4656089.png",
+      "Description": "Police Boat",
+      "DashedNumber": "7287-1 ",
+      "Preselect": "police"
+        },
            
-//     ]
-// };
+    ]
+};
 
 
 
-// var newArray = obj.results.filter(function (el) {
-//   return el.ID ==  2924 &&
-//          el.SetNumber &&
-//          el.URL &&
-//          el.Images &&
-//          el.Description &&
-//          el.DashedNumber &&
-//          el.Preselect ; 
-// });
-// console.log(newArray);
+var newArray = obj.results.filter(function (el) {
+  return el.ID ==  2924 &&
+         el.SetNumber &&
+         el.URL &&
+         el.Images &&
+         el.Description &&
+         el.DashedNumber &&
+         el.Preselect ; 
+});
+console.log(newArray);
 
 
 /////
