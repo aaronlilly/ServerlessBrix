f4f3e3cb532955604a221f2f145209a5c259c351
Aaron Lilly <38325891+aaronlilly@users.noreply.github.com>
2019-03-01 09:14:15 -0500

    Update legoarray.js
    adapting

--- scripts/legoarray.js
+++ scripts/legoarray.js
@@ -3,13 +3,14 @@
 $('#dynam-now').click(function () {
   
 var newArray = obj.results.filter(function (el) {
-  return el.ID ==  $('#dynamId').val() &&
-         el.SetNumber &&
-         el.URL &&
-         el.Images &&
-         el.Description &&
-         el.DashedNumber &&
-         el.Preselect ; 
+    return el.ID == $('#dynamId').val() &&
+        el.SetNumber &&
+        el.URL &&
+        el.Images &&
+        el.Description &&
+        el.Notes &&
+        el.DashedNumber;
+         
 });
   
     $('#ArrayD').append("<a href ='" + newArray[0].URL + "'> <img src='" + newArray[0].Images + "''>"  + newArray[0].ID + " " + newArray[0].Description)
@@ -28,23 +29,24 @@ $(document).ready(function() {
 
 
  var obj = {
-      'results': [{
-      "ID": 2924,
-      "SetNumber": 7942,
-      "URL": "https://www.lego.com/biassets/bi/4586972.pdf",
-      "Images": "https://www.lego.com/biassets/biimg/4586972.png",
-      "Description": "Off Road Fire Rescue",
-      "DashedNumber": "7942-1",
-      "Preselect": "fire"
-        }, {
-              "ID": 4470,
-      "SetNumber": 7942,
-      "URL": "https://www.lego.com/biassets/bi/4656089.pdf",
-      "Images": "https://www.lego.com/biassets/biimg/4656089.png",
-      "Description": "Police Boat",
-      "DashedNumber": "7287-1 ",
-      "Preselect": "police"
-        }
+     'results': [
+         "ID": 1,
+         "SetNumber": 75929,
+         "URL": "https://www.lego.com/biassets/bi/6237211.pdf",
+         "Images": "https://www.lego.com/biassets/biimg/6237211.png",
+         "Description": "Carnotaurus Gyrosphere Escape",
+         "Notes": "BI 3017 / 68+4 - 65/115g, 75929 1/2 V39",
+         "DashedNumber": "75929-1"
+    },
+     {
+         "ID": 2,
+         "SetNumber": 75929,
+         "URL": "https://www.lego.com/biassets/bi/6237212.pdf",
+         "Images": "https://www.lego.com/biassets/biimg/6237212.png",
+         "Description": "Carnotaurus Gyrosphere Escape",
+         "Notes": "No longer listed at LEGO.com",
+         "DashedNumber": "75929-1"
+    }
            
     ]
 };
