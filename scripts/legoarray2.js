6265778f9eeb2f4295bec747ddefa14a6aac5901
Aaron Lilly <aaronlilly84@gmail.com>
2019-02-28 22:46:25 -0500

    trying

--- scripts/legoarray2.js
+++ scripts/legoarray2.js
@@ -11,7 +11,7 @@ var newArray = obj.results.filter(function (el) {
          el.DashedNumber &&
          el.Preselect ; 
 });
-    $('#ArrayD').append(newArray[0] + newArray[1], )
+    $('#ArrayD').append(newArray.results[0] + newArray.results[1], )
 console.log(newArray);
 // $('#ArrayD').append($('#dynamId').val())
 });
