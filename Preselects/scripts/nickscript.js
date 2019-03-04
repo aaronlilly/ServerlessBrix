0521e32c3dac538d5982a7b91ab2833850b16656
Aaron Lilly <aaronlilly84@gmail.com>
2019-03-03 21:05:22 -0500

    add

--- Preselects/scripts/nickscript.js
+++ Preselects/scripts/nickscript.js
@@ -8,12 +8,10 @@
                          }).done(function(data) 
 
                                {
-                        for (var i = 0; i < data.results.length; i++) 
+                        for (var i = 0; i < data.nick.length; i++) 
                         {
-                          
-
-                
-    $("#legoList").append(data.results[i].URL);
+      
+                        $("#legoList").append(data.nick.[i].page);
                         }            
                               });
             });
