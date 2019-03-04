f93f2c5a95f1eb134c4a51bc15047592066df788
Aaron Lilly <aaronlilly84@gmail.com>
2019-03-03 20:51:07 -0500

    test

---
+++ Preselects/scripts/nickscript.js
@@ -0,0 +1,20 @@
+  
+            $(document).ready(function ()  
+            {
+                    $.ajax
+                        ({
+                    method: "GET",
+                    url: "https://aaronlilly.github.io/ServerlessBrix/Preselects/xls/json/batman.json"
+                         }).done(function(data) 
+
+                               {
+                        for (var i = 0; i < data.results.length; i++) 
+                        {
+                          
+
+                
+    $("#legoList").append(data.results[i].URL);
+                        }            
+                              });
+            });
+           
